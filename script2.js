8aa5fa5e1d690cf85ff883184f50e679c0023169
Henri2k5 <henrikyuban@gmail.com>
2025-04-02 02:15:57 -0300

    Atualização

--- script2.js
+++ script2.js
@@ -1,8 +1,37 @@
-function Converter(){
+function Converter() {
+    let valorReal = prompt(
+        "Digite um valor em Reais"
+    );
 
-valorWons = prompt("Digite um valor em Wons")
+    let escolha = prompt(
+        "Escolha para qual moeda deseja converter:\n1 - Won (KRW)\n2 - Dólar (USD)\n3 - Euro (EUR)\n4 - Iene (JPY)"
+    );
 
-umWon = 0.0040
-alert("R$" + valorWons * umWon)
+    let taxaConversao;
+    let moeda;
 
+    switch (escolha) {
+        case "1":
+            taxaConversao = 258; 
+            moeda = "₩";
+            break;
+        case "2":
+            taxaConversao = 0.18;
+            moeda = "US$";
+            break;
+        case "3":
+            taxaConversao = 0.16;
+            moeda = "€";
+            break;
+        case "4":
+            taxaConversao = 26;
+            moeda = "¥";
+            break;
+        default:
+            alert("Opção inválida! Tente novamente.");
+            return;
+    }
+
+    let Resultado = valorReal * taxaConversao;
+    alert(moeda + " " + Resultado.toLocaleString(2));
 }
